d5701e57f467db062ae8ea6dadc6527cdfbf2f40
Alex Jbanca <alexjb@status.im>
2025-12-11 15:50:22 +0200

    fix: Update waiting for keycard text in the keycard drawer

--- ui/i18n/qml_cs.ts
+++ ui/i18n/qml_cs.ts
@@ -9014,10 +9014,6 @@ L2 poplatek: %2</translation>
 <context>
     <name>KeycardChannelDrawer</name>
     <message>
-        <source>Insert Keycard</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Please tap your Keycard to the back of your device</source>
         <translation type="unfinished"></translation>
     </message>
@@ -9047,6 +9043,10 @@ L2 poplatek: %2</translation>
     </message>
     <message>
         <source>Dismiss</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ready to scan</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
