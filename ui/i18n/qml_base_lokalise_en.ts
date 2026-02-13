4fba07fa7970e6ff5477fd44ad882a505443579b
Jonathan Rainville <rainville.jonathan@gmail.com>
2026-02-13 10:37:55 -0500

    fix manifgest and update vendors

--- ui/i18n/qml_base_lokalise_en.ts
+++ ui/i18n/qml_base_lokalise_en.ts
@@ -23788,11 +23788,6 @@
       <comment>main</comment>
       <translation>Report a bug on GitHub</translation>
     </message>
-    <message>
-      <source>Hello World</source>
-      <comment>main</comment>
-      <translation>Hello World</translation>
-    </message>
   </context>
   <context>
     <name>tst_StatusInput</name>
