7bee8f96044f8739e1fb68b0a2fb7c27d3712239
Sale Djenic <aleksandardjenic@status.im>
2026-06-15 12:03:31 +0200

    fix(signing-flow): aligning with yellow papers on v param when signing

--- src/app/modules/shared_modules/signing/module.nim
+++ src/app/modules/shared_modules/signing/module.nim
@@ -1,10 +1,12 @@
-import nimqml, chronicles
+import nimqml, chronicles, strutils
 
 import io_interface
 import view, controller
 import app/core/eventemitter
 import app/modules/shared_models/keypair_item
 
+import app_service/common/wallet_constants as wallet_constants
+import app_service/common/utils as common_utils
 import app_service/service/accounts/service as accounts_service
 import app_service/service/wallet_account/service as wallet_account_service
 import app_service/service/transaction/service as transaction_service
@@ -49,12 +51,31 @@ method getModuleAsVariant*[T](self: Module[T]): QVariant =
 method verifyPassword*[T](self: Module[T], password: string): bool =
   return self.controller.verifyPassword(password)
 
+proc toCanonicalSignature(r, s, v: string): string =
+  let
+    rClean = common_utils.removeHexPrefix(r)
+    sClean = common_utils.removeHexPrefix(s)
+    vClean = common_utils.removeHexPrefix(v)
+  if rClean.len != wallet_constants.SIGNATURE_R_LEN or
+    sClean.len != wallet_constants.SIGNATURE_S_LEN or
+    vClean.len != wallet_constants.SIGNATURE_V_LEN:
+      error "unexpected signature length from signMessage", r=rClean, s=sClean, v=vClean
+      return ""
+  try:
+    let vInt = parseHexInt(vClean)
+    ## Aligning signature with v parameter in yellow-paper form (27/28)
+    let canonicalV = if vInt < 27: vInt + 27 else: vInt
+    return "0x" & rClean & sClean & toLowerAscii(toHex(canonicalV, 2))
+  except ValueError:
+    error "failed to parse v", msg = vClean
+
 method signMessage*[T](self: Module[T], address: string, password: string, txHash: string): string =
   let (res, err) = self.controller.signMessage(address, password, txHash)
   if err.len > 0:
     error "signMessage failed", error=err
     return ""
-  return res
+  let (r, s, v) = common_utils.getRSVFromSignature(res)
+  return toCanonicalSignature(r, s, v)
 
 method isKeypairMigratedToColdWallet*[T](self: Module[T], keyUid: string): bool =
   return self.controller.isKeypairMigratedToColdWallet(keyUid)
@@ -80,6 +101,6 @@ method onKeycardSignFinished*[T](self: Module[T], signature: KeycardSignatureDto
     error "keycard sign error", error=error
     self.view.keycardSignError(error)
     return
-  self.view.keycardSignSuccess(signature.r, signature.s, signature.v)
+  self.view.keycardSignSuccess(toCanonicalSignature(signature.r, signature.s, toHex(signature.v, wallet_constants.SIGNATURE_V_LEN)))
 
 {.pop.}
