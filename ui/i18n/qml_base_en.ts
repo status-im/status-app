243f0fa2311413dd6c8badfaa7e87e982affd1b9
Sale Djenic <aleksandardjenic@status.im>
2026-07-02 09:34:39 +0200

    chore: switch from tenor to klipy gifs
    No default klipy api key set.
    
    Set `STATUS_BUILD_KLIPY_API_KEY` env var locally and rebuild in order to use gifs.
    
    Closes #21372

--- ui/i18n/qml_base_en.ts
+++ ui/i18n/qml_base_en.ts
@@ -3989,11 +3989,11 @@ You will remain logged in, and your recovery phrase will be entirely in your han
 <context>
     <name>ConfirmationPopup</name>
     <message>
-        <source>Enable Tenor GIFs?</source>
+        <source>Enable KLIPY GIFs?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Once enabled, GIFs posted in the chat may share your metadata with Tenor.</source>
+        <source>Once enabled, GIFs posted in the chat may share your metadata with KLIPY.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -6770,7 +6770,7 @@ key pair. Keycard will be required for signing</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Error while contacting Tenor API, please retry.</source>
+        <source>Error while contacting KLIPY API, please retry.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -17180,7 +17180,7 @@ to load</source>
 <context>
     <name>StatusGifPopup</name>
     <message>
-        <source>Search</source>
+        <source>Search KLIPY</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
