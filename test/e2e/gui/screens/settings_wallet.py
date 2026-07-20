215ac3d987266681806d7acb34ea2e7b77be4ee1
Anastasiya <anastasija.ig@gmail.com>
2026-07-20 15:58:18 +0300

    chore(@e2e): remove account ordering tests

--- test/e2e/gui/screens/settings_wallet.py
+++ test/e2e/gui/screens/settings_wallet.py
@@ -607,22 +607,11 @@ class EditAccountOrderSettings(WalletSettingsView):
         super(EditAccountOrderSettings, self).__init__()
         self._account_item = QObject(
             settings_names.settingsContentBaseScrollView_draggableDelegate_StatusDraggableListItem)
-        self._accounts_list = QObject(settings_names.statusDesktop_mainWindow)
-        self._text_item = QObject(settings_names.settingsContentBaseScrollView_StatusBaseText)
-        self._back_button = Button(settings_names.main_toolBar_back_button)
 
     @allure.step('Wait until appears {0}')
     def wait_until_appears(self, timeout_msec: int = configs.timeouts.UI_LOAD_TIMEOUT_MSEC):
         self._account_item.wait_until_appears(timeout_msec)
         return self
-
-    @property
-    @allure.step('Get edit account order recommendations')
-    def account_recommendations(self):
-        account_recommendations = []
-        for obj in driver.findAllObjects(self._text_item.real_name):
-            account_recommendations.append(obj.text)
-        return account_recommendations
 
     @property
     @allure.step('Get accounts')
@@ -655,18 +644,6 @@ class EditAccountOrderSettings(WalletSettingsView):
             if getattr(child, 'objectName', '') == 'show-icon':
                 return child
         raise LookupError(f'Eye icon not found on {name} account item')
-
-    @allure.step('Drag account to change the order')
-    def drag_account(self, name: str, index: int):
-        assert driver.waitFor(lambda: len([account for account in self.accounts if account.name == name]) == 1), \
-            'Account not found or found more then one'
-        bounds = [account for account in self.accounts if account.name == name][0].object.bounds
-        d_bounds = self.accounts[index].object.bounds
-        driver.mouse.press_and_move(self._accounts_list.object, bounds.x, bounds.y, d_bounds.x, d_bounds.y)
-
-    @allure.step('Verify that back button is present')
-    def is_back_button_present(self) -> bool:
-        return self._back_button.is_visible
 
 
 class ManageTokensSettingsView(WalletSettingsView):
