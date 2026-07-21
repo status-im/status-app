07483ad60f297e1dac47da0aa394824142dc960f
Anastasiya <anastasija.ig@gmail.com>
2026-07-21 10:40:27 +0300

    chore(@e2e): use new buttons to edit a message in chat

--- test/e2e/gui/screens/messages.py
+++ test/e2e/gui/screens/messages.py
@@ -641,9 +641,10 @@ class MessageQuickActions(QObject):
         self._delete_button = Button(
             messaging_names.chatMessageViewDelegate_chatDeleteMessageButton_StatusFlatRoundButton)
         self._reply_button = Button(messaging_names.chatMessageViewDelegate_reply_icon_StatusIcon)
-        self._edit_message_field = TextEdit(messaging_names.edit_inputScrollView_messageInputField_TextArea)
+        self._edit_message_input = QObject(messaging_names.chatMessageViewDelegate_editMessageInput_StatusChatInputNew)
+        self._edit_message_field = TextEdit(messaging_names.editMessageInput_messageInputField)
         self._reply_panel = QObject(messaging_names.mainWindow_replyPanel_StatusChatInputReplyPanel)
-        self._save_text_button = Button(messaging_names.chatMessageViewDelegate_Save_StatusButton)
+        self._edit_accept_button = Button(messaging_names.editMessageInput_statusChatInputEditAcceptButton)
         self._message_input_area = TextEdit(messaging_names.inputScrollView_messageInputField_TextArea)
         self._send_message_button = Button(messaging_names.mainWindow_statusChatInputSendButton)
 
@@ -658,8 +659,9 @@ class MessageQuickActions(QObject):
     @allure.step('Edit message and save changes')
     def edit_message(self, text: str):
         self._edit_button.click()
+        self._edit_message_input.wait_until_appears()
         self._edit_message_field.set_text_property(text)
-        self._save_text_button.click()
+        self._edit_accept_button.wait_until_enabled().click()
 
     @allure.step('Delete message')
     def delete_message(self):
