38cd0b93813110ac312e37ec1b62e933ec94dbb7
Alex Jbanca <alexjb@status.im>
2026-08-13 17:03:03 +0300

    chore: update translations

--- ui/i18n/qml_cs.ts
+++ ui/i18n/qml_cs.ts
@@ -3201,13 +3201,6 @@ Přejete si obejít bezpečnostní kontrolu a pokračovat?</translation>
     </message>
 </context>
 <context>
-    <name>ChatsLoadingPanel</name>
-    <message>
-        <source>Loading chats...</source>
-        <translation>Načítání chatů...</translation>
-    </message>
-</context>
-<context>
     <name>CollectibleDetailView</name>
     <message>
         <source>Unknown</source>
