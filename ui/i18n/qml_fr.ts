89b037fe5674f8bc335e82316335226160d9a12e
Alex Jbanca <alexjb@status.im>
2026-08-20 10:26:15 +0300

    chore: update translations

--- ui/i18n/qml_fr.ts
+++ ui/i18n/qml_fr.ts
@@ -3183,13 +3183,6 @@ Do you wish to override the security check and continue?</source>
     </message>
 </context>
 <context>
-    <name>ChatsLoadingPanel</name>
-    <message>
-        <source>Loading chats...</source>
-        <translation>Chargement des conversations...</translation>
-    </message>
-</context>
-<context>
     <name>CollectibleDetailView</name>
     <message>
         <source>Unknown</source>
