706d28c003b8a69e7c059f9ce859452343a93da9
Anastasiya <anastasija.ig@gmail.com>
2026-08-21 15:11:00 +0300

    fix(@e2e): keep AUT ports below the ephemeral range

--- test/e2e/configs/squish.py
+++ test/e2e/configs/squish.py
@@ -1,5 +1,6 @@
 import os
 
-AUT_PORT = 61500 + int(os.getenv('EXECUTOR_NUMBER', 0))
+# Below the OS ephemeral range so the second AUT hook is not stolen by the first instance.
+AUT_PORT = 35100 + int(os.getenv('EXECUTOR_NUMBER', 0))
 SERVER_PORT = 4322 + int(os.getenv('EXECUTOR_NUMBER', 0))
 CURSOR_ANIMATION = False
