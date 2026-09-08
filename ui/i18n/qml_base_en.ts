88f623ccdd8c7c28fcb9f8e2eb455953b98126ea
Alex Jbanca <alexjb@status.im>
2026-09-08 12:08:05 +0300

    fix: Update bot contact request intro message

--- ui/i18n/qml_base_en.ts
+++ ui/i18n/qml_base_en.ts
@@ -1831,10 +1831,6 @@ from &quot;%1&quot; to &quot;%2&quot;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Send the Status Team bot a contact request to get useful Status tips, important updates, and share your feedback, ideas, or issues directly with the Status team.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <source>Invite People</source>
         <translation type="unfinished"></translation>
     </message>
@@ -1999,6 +1995,10 @@ from &quot;%1&quot; to &quot;%2&quot;</source>
     </message>
     <message>
         <source>%1 was removed from your trusted sites.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send a contact request to the Status Team peer-to-peer bot over the Logos network for Status updates and how-to tips, and to share feedback or issues. See our Privacy Policy for more details about interacting with the bot. Disconnect anytime</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
